b1e5a27afff2c1ce0b302667892f7b9f5a470fb6
terryyoungman <youngmant@mymacewan.ca>
2019-01-31 23:32:05 -0700

    add list view for daily entry. NOTE: requires recycler view dependency

--- app/src/main/java/macewan_dust/smiles/DailyItem.java
+++ app/src/main/java/macewan_dust/smiles/DailyItem.java
@@ -1,4 +1,40 @@
 package macewan_dust.smiles;
 
 public class DailyItem {
+
+    private int mIconID;
+    private String mTitle;
+    private String mSubtitle;
+    private int mBackgroundID;
+
+
+
+    /**
+     * constructor
+     * @param iconID
+     * @param title
+     * @param subtitle
+     */
+    public DailyItem(int iconID, int backgroundID, String title, String subtitle) {
+        mIconID = iconID;
+        mTitle = title;
+        mSubtitle = subtitle;
+        mBackgroundID = backgroundID;
+    }
+
+    public int getIconID() {
+        return mIconID;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getSubtitle() {
+        return mSubtitle;
+    }
+
+    public int getBackgroundID() {
+        return mBackgroundID;
+    }
 }
--- app/src/main/java/macewan_dust/smiles/DashboardFragment.java
+++ app/src/main/java/macewan_dust/smiles/DashboardFragment.java
@@ -1,5 +1,6 @@
 package macewan_dust.smiles;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -31,8 +32,17 @@ public class DashboardFragment extends Fragment {
             @Override
             public void onClick(View v){
 
-                DailyFragment fragment = new DailyFragment();
+
+
+                DailyListActivity fragment = new DailyListActivity();
                 replaceFragment(fragment);
+
+
+           //     Intent startDaily = new Intent(getActivity(), DailyListActivity.class);
+           //     startActivity(startDaily);
+
+
+
             }
         });
 
